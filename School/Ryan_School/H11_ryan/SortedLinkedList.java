27267373ea2e28d158299d25f7c886972ff5b0d5
RyanCPeters <petersryan84@gmail.com>
2017-03-07 01:55:28 -0800

    Pushing some updates from h10 and h9 before I send h11

--- School/Ryan_School/H11_ryan/SortedLinkedList.java
+++ School/Ryan_School/H11_ryan/SortedLinkedList.java
@@ -1,68 +1,135 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * @author Ryan Peters
  * @date 3/5/2017
  */
-public class SortedLinkedList<E> implements ISortedList<E>{
+public class SortedLinkedList<E extends Comparable> implements ISortedList<E>{
+	private int size;
+	private Node head;
+	private Node tail;
+
+	public SortedLinkedList() {
+		size = 0;
+		head = head.next = tail = tail.next = null;
+
+	}
+
+	/**returns the number of elements in the list
+	 *
+	 * @return an int value for the number of elements
+	 */
 	@Override
 	public int size() {
-		return 0;
+		return size;
 	}
 
+	/**returns the boolean statement for if the list has no elements and is thus empty.
+	 *
+	 * @return true if the list is empty, otherwise false.
+	 */
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return size == 0;
 	}
 
+	/**returns the first element in the list without removing that element from the list.
+	 *
+	 * @return the first element in the list.
+	 */
 	@Override
 	public E getHead() {
-		return null;
+		return head.value;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
 	public E getTail() {
-		return null;
+		return tail.value;
 	}
 
+	/**returns the first index position of the node in the sorted list that contains the given value, if
+	 * no elements contain that value then this method returns -1.
+	 *
+	 * @param value
+	 * @return
+	 */
 	@Override
 	public int indexOf(E value) {
-		return 0;
+		MyIter iter = (MyIter) iterator();
+		while(iter.hasNext()){
+			E target = iter.next();
+			if(target.compareTo(value) == 0)return iter.getPos();
+		}
+		return -1;
 	}
+
 
 	@Override
 	public boolean contains(E value) {
-		return false;
+		if (indexOf(value) < 0) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public void add(E value) {
+		tail = tail.next = new Node(value);
+		tail.next = null;
+		size++;
 
 	}
 
 	@Override
 	public void addAll(ISortedList<E> other) {
-
+		Iterator<E> iter = other.iterator();
+		while(iter.hasNext()){
+			add(iter.next());
+		}
 	}
 
 	@Override
 	public E removeHead() {
-		return null;
+		E val = head.value;
+		head = head.next;
+		size--;
+		return val;
 	}
 
 	@Override
 	public E removeTail() {
-		return null;
+		E val = tail.value;
+		MyIter iter = (MyIter)iterator();
+		while(iter.hasNext() && iter.getNode().next != tail){
+			iter.next();
+		}
+		tail = iter.getNode();
+		tail.next = null;
+		size--;
+		return val;
 	}
 
 	@Override
 	public boolean remove(E value) {
+		MyIter iter = (MyIter)iterator();
+		while(iter.hasNext()){
+			E check = iter.next();
+			if(check.compareTo(value) == 0){
+				iter.remove();
+				return true;
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public void clear() {
-
+		head = tail = null;
 	}
 
 	/**
@@ -115,6 +182,91 @@ public class SortedLinkedList<E> implements ISortedList<E>{
 	 */
 	@Override
 	public Iterator<E> iterator() {
-		return null;
+		return new MyIter();
+	}
+	private class MyIter implements Iterator<E> {
+		private int pos;
+		private boolean canRemove;
+		private Node curr;
+		private Node prev;
+
+		public MyIter() {
+			this.pos = 0;
+			this.canRemove = false;
+			this.prev = this.curr = head;
+
+		}
+
+
+		protected Node getNode(){return curr;}
+		protected int getPos(){return pos;}
+
+		/**
+		 * Returns true if the iteration has more elements.
+		 * (In other words, returns true if next() would
+		 * return an element rather than throwing an exception.)
+		 *
+		 * @return {@code true} if the iteration has more elements
+		 */
+		@Override
+		public boolean hasNext() {
+			return pos+1 < size;
+		}
+
+		/**
+		 * Returns the next element in the iteration.
+		 *
+		 * @return the next element in the iteration
+		 * @throws NoSuchElementException if the iteration has no more elements
+		 */
+		@Override
+		public E next() throws NoSuchElementException{
+			if(curr == null)return null;
+			if(pos > 0) {
+				prev = curr;
+				curr = curr.next;
+			}
+			pos++;
+
+			return curr.value;
+		}
+
+		/**
+		 * Removes from the underlying collection the last element returned
+		 * by this iterator (optional operation).  This method can be called
+		 * only once per call to {@link #next}.  The behavior of an iterator
+		 * is unspecified if the underlying collection is modified while the
+		 * iteration is in progress in any way other than by calling this
+		 * method.
+		 *
+		 * @throws UnsupportedOperationException if the {@code remove}
+		 *                                       operation is not supported by this iterator
+		 * @throws IllegalStateException         if the {@code next} method has not
+		 *                                       yet been called, or the {@code remove} method has already
+		 *                                       been called after the last call to the {@code next}
+		 *                                       method
+		 * @implSpec The default implementation throws an instance of
+		 * {@link UnsupportedOperationException} and performs no other action.
+		 */
+		@Override
+		public void remove() {
+			if(canRemove){
+				prev.next = curr = curr.next;
+				canRemove = false;
+				size--;
+			}
+		}
+	}
+
+		private class Node {
+		E value;
+		Node next;
+		Node(E value, Node next){
+			this.value = value;
+			this.next = next;
+		}
+		Node(E value){
+			this(value, null);
+		}
 	}
 }
