a7784878fe527e3578abbe5afab31d7bb347825b
Ryan Peters <petersryan84@gmail.com>
2017-04-17 11:33:30 -0700

    Update nroot.m
    Changed the equation used for calculating the root in my initial temporary test set up.
    
     I originally used a sort of newton root method hacky looking thing, now have it as the Modified Babylonian Method derived in class.
    
    I also added more comments describing my thinking in generating the initial guess value.

--- NumericalMethods/nroot.m
+++ NumericalMethods/nroot.m
@@ -5,21 +5,19 @@ function r = nroot(num, n)
 % input: n is the order of the root to which we will take num
 % 
 % output: the n'th root of num as a single double value
-    if num > n;
-        x = num/n;
-    else ;
-        x = n/num;
-    endif;
-    delta = num^(1/n) - x;
-    r = [( x/2 + num/(2*x^(n-1))), 1];
-    for i = 1:1000;
-        if abs(x-r(1)) < .00000000001;
-            r = delta;
-            return ;
-        endif;
+% methodology: Asserting that the given inputs can be expressed
+%              as :: num^(1/n) :: to find the n'th root of num.
+%
+%              We can thus see that the solution will always be
+%              less than the value given by num/n. This assertion
+%              will be the basis for the vaue used in our initial guess.               
+    x = num/n;
+    r = [  (n-1)*x/(2*n) - num/(2*x^(n-1)*n), 1];
+    for i = 2:1000
+        if abs(x-r(1)) < .00000000001
+            return 
+        endif
         x = r(1);
-        r = [x - (x^(n)-num) / ((n)*x^(n-1)) , i];
-        delta = [delta, num^(1/n) - r];
-  endfor;
-  r = delta;
-endfunction;
+        r = [r; (n-1)*x/(2*n) - num/(2*x^(n-1)*n) , i];
+    endfor
+endfunction
